3c0999a28d65174796716d9dbfa9585fc93b4c6d
Becky449 <rabnab12@gmail.com>
2023-05-06 23:48:53 +0300

    validations and test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it { should have_many(:posts).with_foreign_key('author_id') }
+    it { should have_many(:comments).with_foreign_key('author_id') }
+    it { should have_many(:likes).with_foreign_key('author_id') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_numericality_of(:posts_counter).only_integer.is_greater_than_or_equal_to(0) }
+  end
+end
